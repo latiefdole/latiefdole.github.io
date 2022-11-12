dbc023de7a4ec6b8d83e176d45234d004dd10ed7
Abdul Latif <latiefdole@gmail.com>
2022-11-13 01:15:25 +0700

    Add files via upload

---
+++ cache/compiled/files/542d14c1cc4f94e81c8b4fbedc3317eb.yaml.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/xampp8/htdocs/saefuku/user/accounts/stkwildah.yaml',
+    'modified' => 1668275429,
+    'size' => 557,
+    'data' => [
+        'state' => 'enabled',
+        'email' => 'stk.wildah@gmail.com',
+        'fullname' => 'Siti Khotimatul Wildah',
+        'title' => 'Administrator',
+        'language' => 'id',
+        'content_editor' => 'default',
+        'twofa_enabled' => false,
+        'twofa_secret' => 'H4HSZCUXJZRGLTNGMKXYYARWX7U37ACA',
+        'avatar' => [
+            
+        ],
+        'hashed_password' => '$2y$10$Ql0nPuDJSilurEx8GrL4FO6Tv.hssa1bKRWw539zLa6SW8wAsEEUC',
+        'access' => [
+            'site' => [
+                'login' => true
+            ],
+            'admin' => [
+                'login' => true,
+                'super' => true,
+                'cache' => true,
+                'configuration' => true,
+                'pages' => true,
+                'maintenance' => true,
+                'statistics' => true,
+                'plugins' => true,
+                'themes' => true,
+                'tools' => true,
+                'users' => true,
+                'flex-objects' => true
+            ]
+        ]
+    ]
+];
---
+++ cache/compiled/files/b6230404c9995c4aa2078679192abf5c.json.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledJsonFile',
+    'filename' => 'C:/xampp8/htdocs/saefuku/user/data/flex/indexes/pages.json',
+    'modified' => 1668274233,
+    'size' => 635,
+    'data' => [
+        'version' => '1.5',
+        'timestamp' => 1668274233,
+        'count' => 3,
+        'index' => [
+            '' => [
+                'key' => '',
+                'storage_key' => '',
+                'template' => NULL,
+                'storage_timestamp' => 1668273858,
+                'children' => [
+                    '01.home' => 1668273858,
+                    '02.typography' => 1668273858
+                ],
+                'checksum' => 'd96f2779c70426e9e39454ca552bec64'
+            ],
+            '01.home' => [
+                'key' => 'home',
+                'storage_key' => '01.home',
+                'template' => 'default',
+                'storage_timestamp' => 1668273858,
+                'markdown' => [
+                    '' => [
+                        'default' => 1668273858
+                    ]
+                ],
+                'checksum' => '01ebd4aa30fd88a3f9d94d7555fa8b82'
+            ],
+            '02.typography' => [
+                'key' => 'typography',
+                'storage_key' => '02.typography',
+                'template' => 'default',
+                'storage_timestamp' => 1668273858,
+                'markdown' => [
+                    '' => [
+                        'default' => 1668273858
+                    ]
+                ],
+                'checksum' => '7b79f226cba2cf1839f28b784762086c'
+            ]
+        ]
+    ]
+];
